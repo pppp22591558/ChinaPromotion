ee1773dda8fa99c3388e94b791530e40f5a3b388
NeilZheng <pppp29654213@gmail.com>
2016-08-31 13:02:41 +0800

    use just font

--- src/components/Html/Html.js
+++ src/components/Html/Html.js
@@ -64,6 +64,8 @@ class Html extends Component {
         <link rel="alternate" href="http://app.pagamo.org/en" hrefLang="en" />
         <link rel="alternate" href="http://app.pagamo.org/cn" hrefLang="zh-Hans" />
         <link rel="alternate" href="http://app.pagamo.org/tw" hrefLang="zh-Hant" />
+        <link href='//fonts.googleapis.com/css?family=Source+Sans Pro:400|Varela+Round:400' rel='stylesheet' type='text/css' />
+        <script src="//s3-ap-northeast-1.amazonaws.com/justfont-user-script/jf-33366.js"></script>
         <style id="css" dangerouslySetInnerHTML={{__html: this.props.css}} />
         <script dangerouslySetInnerHTML={this.TEST_SETUP()} />
         <script dangerouslySetInnerHTML={this.TEST_SEND()} />
